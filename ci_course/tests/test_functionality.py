de12c78d2611c2e6cf7f8701172a164e4dcf838a
Theresa Burkard <94674809+tiozab@users.noreply.github.com>
2026-02-05 16:25:20 +0000

    Fix minimum function test for string input

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,5 +16,5 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
-    assert ci_course.minimum('0') == 
+    assert ci_course.minimum('0') is None
     
